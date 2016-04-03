d0dc95b00ecc1ca91215bf4ef02c7ff75b98d1fd
Tianpei <tianpei@umich.edu>
2016-04-03 09:39:48 -0400

    add timestamp

--- src/average_degree.py
+++ src/average_degree.py
@@ -2,9 +2,18 @@
 from __future__ import with_statement
 import os, sys
 import json
+#from Queue import Queue
+from collections import deque
 import getopt
 from dateutil.parser import parse
 from datetime import datetime,timedelta
+
+class HashtagState:
+    def __init__(self, created_at, degree_increase):
+       self.created_at = created_at 
+       self.degree_increase = degree_increase
+#       for hashtag in hashtags:
+#           self.queue_node = 
 
 class HashtagGraph:
    '''
@@ -15,8 +24,12 @@ class HashtagGraph:
        self.num_nodes = 0
        self.num_total_degree = 0
        self.hash_nodeDegree = {}
-       self.hash_nodeTime = {}
+       self.hash_nodeIncrease = {}
        self.list_avg_Degree = []
+       self.maximum_timestamp = parse("Mon Jan 01 00:00:00 +0000 1000", fuzzy=True)
+       self.minimum_timestamp = parse("Mon Jan 01 00:00:00 +0000 3000", fuzzy=True)
+
+
 
 #   def graph_construct(self, input_filename):
 #       self.inputfile_open(input_filename)
@@ -31,18 +44,29 @@ class HashtagGraph:
      
         This is the interface for running the graph_grow and graph_prune
        '''
-       self.inputfile_open(input_filename)
+       if(self.inputfile_open(input_filename) == False):
+           return 
+ 
        for tweet_line in self.inputfile:
            # parse tweet line
-           hashtags, created_ats = parse_tweet(tweet_line)
+           hashtags, created_at = parse_tweet(tweet_line)
+           if parse_time(created_at) > self.maximum_timestamp:
+               self.maximum_timestamp = parse_time(created_at)
+           #if parse_time(created_at) < self.minimum_timestamp:
+           #    self.minimum_timestamp = parse_time(created_at)
+           # see if the created_at is over the 60 second slot
+           # self.queue_hist.put(HashtagState(hashtags, created_at))
+           # add code here 
+           # 
            # grow graph
-           self.graph_grow(hashtags)
+           self.graph_grow(hashtags, created_at)
            self.list_avg_Degree.append(float(self.num_total_degree) / float(self.num_nodes))
-           #add code here
-       
+          
+        
+       self.inputfile.close()
 
 
-   def graph_grow(self, hashtags):
+   def graph_grow(self, hashtags, created_at):
        '''
 
           use the hash table to store the total degree for each hashtag
@@ -56,7 +80,8 @@ class HashtagGraph:
        num_newnodes = self.count_new_nodes(hashtags)
        if num_newnodes == 0:
           return
-
+       
+       timestamp = parse_time(created_at)
        print("Add {0:3d} nodes".format(num_newnodes))
        self.num_nodes = self.num_nodes + num_newnodes
        for hashtag in hashtags:
@@ -65,12 +90,29 @@ class HashtagGraph:
            except KeyError:
                self.hash_nodeDegree[hashtag] = len(hashtags) - 1
                self.num_total_degree = self.num_total_degree + len(hashtags) - 1 
+               #store the incremental degree and time
+               #q_temp = Queue()
+               s_temp = HashtagState(created_at = timestamp, degree_increase = len(hashtags) - 1)
+               q_temp = deque([s_temp])
+               #q_temp.put(s_temp)
+               self.hash_nodeIncrease[hashtag] = q_temp
            else:
-               self.num_total_degree = self.num_total_degree + num_newnodes
+               self.num_total_degree = self.num_total_degree + num_newnodes 
+               #store the incremental degree and time
+               s_temp = HashtagState(created_at = timestamp, degree_increase = num_newnodes)
+               self.hash_nodeIncrease[hashtag].append(s_temp)
+               
+               #self.hash_nodeIncrease[hashtag].put(s_temp)
        return
 
    def graph_prune(self, time_stamp):
+       '''
+
+           pruning of the graph 
+       '''
        return
+
+
 
    def count_new_nodes(self, hashtags):
        '''
@@ -85,41 +127,49 @@ class HashtagGraph:
        num_newnodes = len(set(hashtags) - set(node_keys) )
        return num_newnodes
 
+   def empty(self):
+       self.hash_nodeDegree = {}
+       self.num_nodes = 0
+       self.num_total_degree = 0
+       self.maximum_timestamp = parse("Mon Jan 01 00:00:00 +0000 1000", fuzzy=True)
+       self.minimum_timestamp = parse("Mon Jan 01 00:00:00 +0000 3000", fuzzy=True)
+       #self.queue_hist.empty()
+
+
    def inputfile_open(self, input_filename):
        try:
             self.inputfile = open(input_filename , 'r' )
        except EnvironmentError:
             print 'open inputfile failure'
+            return False
+       else:
+            return True
 
    def outputfile_open(self, output_filename):
        try:
             open(output_filename,"w").close()
        except EnvironmentError:
             print 'open outputfile failure'
+            return False
        else:
             self.outputfile = open(output_filename , 'w')
-
+            return True
 
    def write_txt(self, output_filename):
        '''
 
             Write to given file
        '''
-       self.outputfile_open(output_filename)
+       if(self.outputfile_open(output_filename) == False):
+           return
        #self.outputfile.write("\n".join(str(self.list_avg_Degree)))
        for num in self.list_avg_Degree:
-            print >> self.outputfile, "{0:.3f}".format(num)
-
-
-   def file_close(self):
-       try:
-           self.inputfile.close()
-       except AttributeError:
-           print("no input")
+           print >> self.outputfile, "{0:.3f}".format(num)
        try:
            self.outputfile.close()
        except AttributeError:
            print("no output")
+
 
    def __str__(self): 
        print("Number of nodes: {0:5d}".format(self.num_nodes))
@@ -190,7 +240,6 @@ def main(argv):
     HashGraphObj.run(inputfile)
     print("Write to " + outputfile)
     HashGraphObj.write_txt(outputfile)
-    HashGraphObj.file_close()
 
 
 
