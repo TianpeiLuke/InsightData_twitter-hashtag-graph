ec53c110574588a4e3419c6b8e7ff5e40d24e705
Tianpei <tianpei@umich.edu>
2016-04-04 16:01:46 -0400

    change the bash code

--- run.sh
+++ run.sh
@@ -3,16 +3,22 @@
 # example of the run script for running the word count
 
 # I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
-echo "Type the naive implementation [n] or the hash implmentation [h], followed by [ENTER]:"
+echo "Type the input file name under './tweet_input/':"
+read input_file
+
+echo "Type the output file name under './tweet_output/':"
+read output_file
+
+echo "Type [n] for the naive implementation or [h] for the hash implmentation, followed by [ENTER]:"
 
 read choice
 
 if [ $choice == n ]; then
    echo "Naive implementation"
-   time python ./src/average_degree_naive.py -i ./tweet_input/tweets.txt -o ./tweet_output/output.txt
+   time python ./src/average_degree_naive.py -i ./tweet_input/$input_file -o ./tweet_output/$output_file
 elif [ $choice == h ]; then
    echo "Hash-table implementation"
-   time python ./src/average_degree_hash.py -i ./tweet_input/tweets.txt -o ./tweet_output/output.txt
+   time python ./src/average_degree_hash.py -i ./tweet_input/$input_file -o ./tweet_output/$output_file
 else
    echo "Choose [n] or [h]"
 fi 
