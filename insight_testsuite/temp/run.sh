9e5025406cefaf372247aa406b07e877b6a1886b
Tianpei <tianpei@umich.edu>
2016-04-04 17:16:31 -0400

    change run file

--- insight_testsuite/temp/run.sh
+++ insight_testsuite/temp/run.sh
@@ -3,11 +3,11 @@
 # example of the run script for running the word count
 
 # I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
-echo "Type the input file name under './tweet_input/':"
-read input_file
+#echo "Type the input file name under './tweet_input/':"
+#read input_file
 
-echo "Type the output file name under './tweet_output/':"
-read output_file
+#echo "Type the output file name under './tweet_output/':"
+#read output_file
 
 echo "Type [n] for the naive implementation or [h] for the hash implmentation, followed by [ENTER]:"
 
@@ -15,10 +15,10 @@ read choice
 
 if [ $choice == n ]; then
    echo "Naive implementation"
-   time python ./src/average_degree_naive.py -i ./tweet_input/$input_file -o ./tweet_output/$output_file
+   time python ./src/average_degree_naive.py -i ./tweet_input/tweets.txt -o ./tweet_output/output.txt
 elif [ $choice == h ]; then
    echo "Hash-table implementation"
-   time python ./src/average_degree_hash.py -i ./tweet_input/$input_file -o ./tweet_output/$output_file
+   time python ./src/average_degree_hash.py -i ./tweet_input/tweets.txt -o ./tweet_output/output.txt
 else
    echo "Choose [n] or [h]"
 fi 
